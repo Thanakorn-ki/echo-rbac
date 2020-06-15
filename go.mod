69275bc6207038786f57a7cd99eba7b06328c641
thanakorn.ki <thanakorn.ki@linecorp.com>
2020-06-15 23:40:05 +0700

    feat: create RBAC with golang

--- go.mod
+++ go.mod
@@ -3,14 +3,12 @@ module github.com/Thanakorn-ki/echo-rbac
 go 1.13
 
 require (
-	github.com/casbin/casbin v1.9.1 // indirect
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/casbin/casbin v1.9.1
+	github.com/casbin/casbin/v2 v2.0.0
 	github.com/labstack/echo v3.3.10+incompatible // indirect
-	github.com/labstack/gommon v0.3.0 // indirect
-	github.com/mattn/go-colorable v0.1.6 // indirect
-	github.com/valyala/fasttemplate v1.1.0 // indirect
+	github.com/labstack/echo-contrib v0.9.0
+	github.com/labstack/echo/v4 v4.1.16
 	golang.org/x/crypto v0.0.0-20200604202706-70a84ac30bf9 // indirect
 	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
 	golang.org/x/sys v0.0.0-20200610111108-226ff32320da // indirect
-	golang.org/x/text v0.3.2 // indirect
 )
--- main.go
+++ main.go
@@ -1,20 +1,65 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/casbin/casbin/v2"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
+	eg, _ := casbin.NewEnforcer("auth_model.conf", "auth_policy.csv")
+
+	e.Use(Middleware(eg))
 
 	e.Any("*", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return c.String(http.StatusOK, http.StatusText(http.StatusOK))
 	})
 
 	e.Logger.Fatal(e.Start(":1323"))
+}
 
-	fmt.Print("sdssd")
+type (
+	// Config defines the config for CasbinAuth middleware.
+	Config struct {
+		// Enforcer CasbinAuth main rule.
+		// Required.
+		Enforcer *casbin.Enforcer
+	}
+)
+
+func Middleware(ce *casbin.Enforcer) echo.MiddlewareFunc {
+	config := Config{
+		Enforcer: ce,
+	}
+
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			if pass, err := config.CheckPermission(c); err == nil && pass {
+				return next(c)
+			} else if err != nil {
+				return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+			}
+
+			return echo.ErrForbidden
+		}
+	}
+}
+
+// GetUserName gets the user name from the request.
+// Currently, only HTTP basic authentication is supported
+func (a *Config) GetUserName(c echo.Context) string {
+	username, _, _ := c.Request().BasicAuth()
+	return username
+}
+
+// CheckPermission checks the user/path/method combination from the request.
+// Returns true (permission granted) or false (permission forbidden)
+func (a *Config) CheckPermission(c echo.Context) (bool, error) {
+	user := a.GetUserName(c)
+	path := c.Request().URL.Path
+	method := c.Request().Method
+	return a.Enforcer.Enforce(user, path, method)
 }
